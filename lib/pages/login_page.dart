87c82b871dcf7e252d3e2656171b239b8675519d
Muhammad Ali Ghaffar <55552043+MuhammadAliGhaffar@users.noreply.github.com>
2021-09-12 15:25:16 +0500

    day 6 work (#5)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,7 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_catelog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changedButton = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -16,7 +24,7 @@ class LoginPage extends StatelessWidget {
               ),
               SizedBox(height: 20),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 20),
@@ -28,6 +36,11 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: InputDecoration(
                           hintText: "Enter username", labelText: "Username"),
+                      onChanged: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -37,16 +50,72 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                     SizedBox(height: 40),
-                    ElevatedButton(
-                        child: Text("Login"),
-                        style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                        onPressed: () {
-                          /**
-                           * Going from one page to another page
-                           */
-                          //Navigator.push(context, route)
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        })
+                    /**
+                     * Custom Container with Animation
+                     */
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changedButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: 150,
+                        height: 40,
+                        alignment: Alignment.center,
+                        child: changedButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius: BorderRadius.circular(8.0)),
+                      ),
+                    ),
+                    /**
+                     * Custom Container without Animation
+                     */
+                    // InkWell(
+                    //   onTap: () {
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: Container(
+                    //     width: 150,
+                    //     height: 40,
+                    //     alignment: Alignment.center,
+                    //     child: Text(
+                    //       "Login",
+                    //       style: TextStyle(
+                    //           color: Colors.white,
+                    //           fontSize: 16,
+                    //           fontWeight: FontWeight.bold),
+                    //     ),
+                    //     decoration: BoxDecoration(
+                    //         color: Colors.deepPurple,
+                    //         borderRadius: BorderRadius.circular(8.0)),
+                    //   ),
+                    // ),
+                    // ElevatedButton(
+                    //     child: Text("Login"),
+                    //     style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    //     onPressed: () {
+                    //       /**
+                    //        * Going from one page to another page
+                    //        */
+                    //       //Navigator.push(context, route)
+                    //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //     })
                   ],
                 ),
               )
