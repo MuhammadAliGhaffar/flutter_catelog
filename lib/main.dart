6469a558aba56fa8a4c500af0d66c892d2e5a389
Muhammad Ali Ghaffar <55552043+MuhammadAliGhaffar@users.noreply.github.com>
2021-09-17 02:52:45 -0700

    Day 9 (#8)
    * theme extracted
    
    * adding models

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catelog/pages/home_page.dart';
 import 'package:flutter_catelog/pages/login_page.dart';
 import 'package:flutter_catelog/utils/routes.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_catelog/widgets/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,10 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: MyThemeData.lightTheme,
+      darkTheme: MyThemeData.darkTheme,
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
---
+++ lib/models/catelog.dart
@@ -0,0 +1,7 @@
+class Item {
+  final String id, name, description, color, imageUrl;
+  final num price;
+
+  Item(this.id, this.name, this.description, this.color, this.imageUrl,
+      this.price);
+}
