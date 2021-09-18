84427429f6aee3b7081c02f8f3b14c06782b8283
Muhammad Ali Ghaffar <55552043+MuhammadAliGhaffar@users.noreply.github.com>
2021-09-18 02:11:57 -0700

    day 10 (#9)

--- lib/models/catelog.dart
+++ lib/models/catelog.dart
@@ -1,7 +1,26 @@
 class Item {
-  final String id, name, description, color, imageUrl;
-  final num price;
+  final String? name, description, color, imageUrl;
+  final num? price;
+  final int? id;
 
-  Item(this.id, this.name, this.description, this.color, this.imageUrl,
-      this.price);
+  Item(
+      {this.id,
+      this.name,
+      this.description,
+      this.color,
+      this.imageUrl,
+      this.price});
+}
+
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        description: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        imageUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  ];
 }
